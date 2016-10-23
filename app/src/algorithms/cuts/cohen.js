f4894df3ee389144d29e36f7ff4052e6d00d8e6b
HanMufu <hanmufu@gmail.com>
2016-10-23 21:01:54 +0800

    repair cutting algorithm (#2)
    * repair cutting algorithm
    
    * repair cut algorithm

--- app/src/algorithms/cuts/cohen.js
+++ app/src/algorithms/cuts/cohen.js
@@ -21,17 +21,17 @@ function Encode(pt, left, right) {
     ptx = ptx | R;
   }
 
-  if (pt.y > right.y) {
+  if (pt.y < right.y) {
     ptx = ptx | B;
-  } else if (pt.y < left.y) {
+  } else if (pt.y > left.y) {
     ptx = ptx | T;
   }
   return ptx;
 }
   // ************** end of 给直线的端点编码 ***************
 
-  // function wantpush(lineptx1, lineptx2) {
-  //   for (var i = lineptx2; i >= lineptx1; i--) {
+  // function wantpush(code1, code2) {
+  //   for (var i = code2; i >= code1; i--) {
   //     let push1 = [];
   //     push1.push();
   //   }
@@ -48,20 +48,20 @@ export default ({ selectedPixels }) => {
   right = { ...right };
   [left.x, left.y, right.x, right.y] = [
     Math.min(left.x, right.x),
-    Math.min(left.y, right.y),
+    Math.max(left.y, right.y),
     Math.max(left.x, right.x),
-    Math.max(left.y, right.y)
+    Math.min(left.y, right.y)
   ];
 
-  let lineptx1 = 0;
-  let lineptx2 = 0;
-  lineptx1 = Encode(line1, left, right);
-  lineptx2 = Encode(line2, left, right);
-  // lineptx1和2是直线两个端点的编码
+  let code1 = 0;
+  let code2 = 0;
+  code1 = Encode(line1, left, right);
+  code2 = Encode(line2, left, right);
+  // code1和2是直线两个端点的编码
 
   // ********用编码判断直线是否需要裁剪并进行绘制***********
 
-  // ***计算直线与各边的交点***
+  // ************* 计算直线与各边的交点 *************
   let rightside = { x: 0, y: 0 };
   let leftside = { x: 0, y: 0 };
   let topside = { x: 0, y: 0 };
@@ -83,17 +83,17 @@ export default ({ selectedPixels }) => {
   topside.y = Math.round(topside.y);
   bottomside.x = Math.round(bottomside.x);
   bottomside.y = Math.round(bottomside.y);
-  // ***计算直线与各边的交点***
+  // ************* end of 计算直线与各边的交点 *************
 
-  if (lineptx1 & lineptx2) {
+  if (code1 & code2) {
     return { outline: [], fills: [] };
   }
-  if (lineptx1 === 0 && lineptx2 === 0) {
+  if (code1 === 0 && code2 === 0) {
     return dda({ selectedPixels: [line1, line2] });
   }
 
 // *********** 判断起点的选择 ***********
-  switch (lineptx1) {
+  switch (code1) {
     case T: {
       line1.y = left.y;
       line1.x = topside.x;
@@ -119,25 +119,29 @@ export default ({ selectedPixels }) => {
         line1.x = topside.x;
         line1.y = left.y;
         break;
-      } else if (leftside.y >= left.y && leftside.y <= right.y) {
+      } else if (leftside.y <= left.y && leftside.y >= right.y) {
         line1.y = leftside.y;
         line1.x = left.x;
         break;
-      } else break;
+      } else {
+        return { outline: [], fills: [] };
+      }
     }
     case RT: {
       if (topside.x >= left.x && topside.x <= right.x) {
         line1.x = topside.x;
         line1.y = left.y;
         break;
-      } else if (leftside.y >= left.y && leftside.y <= right.y) {
+      } else if (leftside.y <= left.y && leftside.y >= right.y) {
         line1.y = leftside.y;
         line1.x = right.x;
         break;
-      } else break;
+      } else {
+        return { outline: [], fills: [] };
+      }
     }
     case RB: {
-      if (leftside.y >= left.y && leftside.y <= right.y) {
+      if (leftside.y <= left.y && leftside.y >= right.y) {
         line1.y = leftside.y;
         line1.x = right.x;
         break;
@@ -145,24 +149,28 @@ export default ({ selectedPixels }) => {
         line1.x = bottomside.x;
         line1.y = right.y;
         break;
-      } else break;
+      } else {
+        return { outline: [], fills: [] };
+      }
     }
     case LB: {
       if (bottomside.x >= left.x && bottomside.x <= right.x) {
         line1.x = bottomside.x;
         line1.y = right.y;
         break;
-      } else if (leftside.y >= left.y && leftside.y <= right.y) {
+      } else if (leftside.y <= left.y && leftside.y >= right.y) {
         line1.y = leftside.y;
         line1.x = left.x;
         break;
-      } else break;
+      } else {
+        return { outline: [], fills: [] };
+      }
     }
   }
 // ************ end of 判断起点的选择 ***********
 
 // *********** 判断终点的选择 ***********
-  switch (lineptx2) {
+  switch (code2) {
     case T: {
       line2.y = left.y;
       line2.x = topside.x;
@@ -188,25 +196,29 @@ export default ({ selectedPixels }) => {
         line2.x = topside.x;
         line2.y = left.y;
         break;
-      } else if (leftside.y >= left.y && leftside.y <= right.y) {
+      } else if (leftside.y <= left.y && leftside.y >= right.y) {
         line2.y = leftside.y;
         line2.x = left.x;
         break;
-      } else break;
+      } else {
+        return { outline: [], fills: [] };
+      }
     }
     case RT: {
       if (topside.x >= left.x && topside.x <= right.x) {
         line2.x = topside.x;
         line2.y = left.y;
         break;
-      } else if (leftside.y >= left.y && leftside.y <= right.y) {
+      } else if (leftside.y <= left.y && leftside.y >= right.y) {
         line2.y = leftside.y;
         line2.x = right.x;
         break;
-      } else break;
+      } else {
+        return { outline: [], fills: [] };
+      }
     }
     case RB: {
-      if (leftside.y >= left.y && leftside.y <= right.y) {
+      if (leftside.y <= left.y && leftside.y >= right.y) {
         line2.y = leftside.y;
         line2.x = right.x;
         break;
@@ -214,27 +226,30 @@ export default ({ selectedPixels }) => {
         line2.x = bottomside.x;
         line2.y = right.y;
         break;
-      } else break;
+      } else {
+        return { outline: [], fills: [] };
+      }
     }
     case LB: {
       if (bottomside.x >= left.x && bottomside.x <= right.x) {
         line2.x = bottomside.x;
         line2.y = right.y;
         break;
-      } else if (leftside.y >= left.y && leftside.y <= right.y) {
+      } else if (leftside.y <= left.y && leftside.y >= right.y) {
         line2.y = leftside.y;
         line2.x = left.x;
         break;
-      } else break;
+      } else {
+        return { outline: [], fills: [] };
+      }
     }
   }
 // *********** end of 判断终点的选择 ***********
 
+  if (line1.x > right.x || line1.x < left.x || line1.y > left.y || line1.y < right.y || line2.x > right.x || line2.x < left.x || line2.y > left.y || line2.y < right.y) {
+    return { outline: [], fills: [] };
+  }
+
   return dda({ selectedPixels: [line1, line2] });
 
-  // if (lineptx1 | lineptx2 === 0) {
-  //   return dda({ selectedPixels: [line1, line2] });
-  // }
-
-  // return { outline,fills };
 };
